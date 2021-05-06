15aea23b74790abc8ce06562f95d8308f5835078
Owen Green <gungwho@gmail.com>
2021-05-06 01:12:36 +0100

    add js max class wrapper

---
+++ jsextensions/maxclasswrap.js
@@ -0,0 +1,15 @@
+maxclasswrap("fluid.dataset~jswrap", "FluidDataSet");
+maxclasswrap("fluid.labelset~jswrap", "FluidLabelSet");
+maxclasswrap("fluid.kdtree~jswrap", "FluidKDTree");
+maxclasswrap("fluid.kmeans~jswrap", "FluidKMeans");
+maxclasswrap("fluid.knnclassifier~jswrap", "FluidKNNClassifier");
+maxclasswrap("fluid.knnregressor~jswrap", "FluidKNNRegressor");
+maxclasswrap("fluid.normalize~jswrap", "FluidNormalize");
+maxclasswrap("fluid.standardize~jswrap", "FluidStandardize");
+maxclasswrap("fluid.robustscale~jswrap", "FluidRobustscale");
+maxclasswrap("fluid.pca~jswrap", "FluidPCA");
+maxclasswrap("fluid.mds~jswrap", "FluidMDS");
+maxclasswrap("fluid.umap~jswrap", "FluidUMAP");
+maxclasswrap("fluid.datasetquery~jswrap", "FluidDataSetQuery");
+maxclasswrap("fluid.mlpregressor~jswrap", "FluidMLPRegressor");
+maxclasswrap("fluid.mlpclassifier~jswrap", "FluidMLPClassifier");
